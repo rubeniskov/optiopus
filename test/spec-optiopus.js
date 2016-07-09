5e0bb2f37fd2cff5ce33c18792c90102d57f360a
Rubén López Gómez <loslopez1@icloud.com>
2016-07-10 01:49:54 +0200

    update: improve performance and structure

--- test/spec-optiopus.js
+++ test/spec-optiopus.js
@@ -18,26 +18,22 @@ describe('Optiopus', function() {
     });
 
     it('should get the new value of the property keeping default structure', function() {
-
       var defs = optiopus(defaults),
           opts = defs.options({
               'color': '#FF0000'
           });
 
           opts.option('font.size', '11px');
-
           opts.option({
             'background.color': 'hsla(100,10,1,1)',
             'padding.0': '10px',
             'padding.1': '15px'
           });
-
-
         expect(opts.option('color')).to.be.equal('#FF0000');
-        expect(opts.options['color']).to.be.equal('#FF0000');
-        expect(opts.options['font']['family']).to.be.equal('Arial');
+        expect(opts['color']).to.be.equal('#FF0000');
+        expect(opts['font']['family']).to.be.equal('Arial');
         expect(opts.option('font.size')).to.be.equal('11px');
         expect(opts.option('background')).to.has.property('color');
-        expect(opts.options['padding'][0]).to.be.equal('10px');
+        expect(opts['padding'][0]).to.be.equal('10px');
     });
 });
